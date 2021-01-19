02358407356a2fd676bb813c1bf1b5f9d7b602f8
bezdomnij <attlila.novak@yahoo.co.uk>
2021-01-19 20:23:51 +0100

    restructure apptest

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,31 +1,24 @@
 package org.example;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-
-
 import org.junit.Test;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
-
 import java.net.MalformedURLException;
 import java.net.URL;
 
-
 public class AppTest {
     static WebDriver driver;
+    private final String nodeUrl
+            = "https://selenium:CoolCanvas19.@seleniumhub.codecool.codecanvas.hu/wd/hub";
 
     @Test
     public void shouldAnswerWithTrue() throws MalformedURLException {
-        String nodeUrl = "https://selenium:CoolCanvas19.@seleniumhub.codecool.codecanvas.hu/wd/hub";
         DesiredCapabilities capabilities = DesiredCapabilities.chrome();
         capabilities.setBrowserName("chrome");
         capabilities.setPlatform(Platform.LINUX);
-
         driver = new RemoteWebDriver(new URL(nodeUrl), capabilities);
         String practiceUrl = "https://www.google.com/";
         driver.get(practiceUrl);
@@ -37,20 +30,17 @@ public class AppTest {
 
     @Test
     public void shouldAnswerWithTrue2() throws MalformedURLException {
-        String nodeUrl = "https://selenium:CoolCanvas19.@seleniumhub.codecool.codecanvas.hu/wd/hub";
         DesiredCapabilities capabilities = DesiredCapabilities.firefox();
         capabilities.setBrowserName("firefox");
         capabilities.setPlatform(Platform.LINUX);
-
         driver = new RemoteWebDriver(new URL(nodeUrl), capabilities);
-        String practiceUrl = "https://www.google.com/";
+        String practiceUrl = "https://www.bbc.com/";
         driver.get(practiceUrl);
         String currentUrl = driver.getCurrentUrl();
         System.out.println(currentUrl);
         driver.quit();
         assertEquals(practiceUrl, currentUrl);
     }
-
 }
 
 
