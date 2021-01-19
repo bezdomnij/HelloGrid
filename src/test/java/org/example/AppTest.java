6f8f190d7cac7817adef7dfcb8635b07fcf13b1d
David Kovacs <kdavid.302010@gmail.com>
2021-01-19 14:50:51 +0100

    .

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 
-import org.example.Util.Utils;
+
 import org.junit.Test;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
