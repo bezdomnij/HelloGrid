aeab7357fecf19b0362782f5ea058fbe95a30e58
David Kovacs <kdavid.302010@gmail.com>
2021-01-19 14:20:03 +0100

    Initial commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,60 @@
+package org.example;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+import org.example.Util.Utils;
+import org.junit.Test;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class AppTest 
+{
+    static WebDriver driver;
+
+    /*@BeforeTest
+    public void setUp()throws MalformedURLException {
+        final String WEBDRIVER = "webdriver.chrome.driver";
+        final String CHROME_DRIVER_LOCATION = "/home/david/Letöltések";
+        System.setProperty(WEBDRIVER, CHROME_DRIVER_LOCATION);
+        String nodeUrl = "https://seleniumhub.codecool.codecanvas.hu/wd/hub";
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setBrowserName("chrome");
+        capabilities.setPlatform(Platform.LINUX);
+
+        driver = new RemoteWebDriver(new URL(nodeUrl), capabilities);
+    }*/
+
+
+    @Test
+    public void shouldAnswerWithTrue() throws MalformedURLException {
+        String nodeUrl = "https://selenium:CoolCanvas19.@seleniumhub.codecool.codecanvas.hu/wd/hub";
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setBrowserName("chrome");
+        capabilities.setPlatform(Platform.LINUX);
+
+        driver = new RemoteWebDriver(new URL(nodeUrl), capabilities);
+        String practiceUrl = "https://www.google.com/";
+        driver.get(practiceUrl);
+        String currentUrl = driver.getCurrentUrl();
+        System.out.println(currentUrl);
+        driver.quit();
+        assertEquals(practiceUrl, currentUrl);
+    }
+
+    @AfterTest
+    public void afterTest(){
+        driver.quit();
+    }
+}
+
+
