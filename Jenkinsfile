c7e51c42e7c0aeb5f391e19fc9f8f39841e537cc
bezdomnij <attlila.novak@yahoo.co.uk>
2021-01-20 23:11:34 +0100

    5

--- Jenkinsfile
+++ Jenkinsfile
@@ -6,13 +6,18 @@ pipeline {
     }
     stages {
         stage ('Build'){
-
             steps {
                 sh 'mvn --version'
+                sh 'mvn clean'
                 sh 'echo $BROWSER_USED'
                 sh 'echo $SEL_USER'
                 sh 'printenv | sort'
                 }
+        }
+        stage {
+            steps {
+                sh 'mvn test'
+            }
         }
     }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,13 +1,15 @@
 package org.example;
 
-import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+
 import java.net.MalformedURLException;
 import java.net.URL;
+
+import static org.junit.Assert.*;
 
 public class AppTest {
     static WebDriver driver;
@@ -16,9 +18,20 @@ public class AppTest {
 
     @Test
     public void shouldAnswerWithTrue() throws MalformedURLException {
-        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-        capabilities.setBrowserName("chrome");
-        capabilities.setPlatform(Platform.LINUX);
+        String browser = System.getenv("BROWSER");
+        DesiredCapabilities capabilities = null;
+        if (browser.equals("chrome")) {
+            capabilities = DesiredCapabilities.chrome();
+            capabilities.setBrowserName("chrome");
+            capabilities.setPlatform(Platform.LINUX);
+        } else if (browser.equals("firefox")) {
+            capabilities = DesiredCapabilities.firefox();
+            capabilities.setBrowserName("firefox");
+            capabilities.setPlatform(Platform.LINUX);
+        } else {
+            System.out.println("No such browser");
+            fail();
+        }
         driver = new RemoteWebDriver(new URL(nodeUrl), capabilities);
         String practiceUrl = "https://www.google.com/";
         driver.get(practiceUrl);
@@ -28,19 +41,19 @@ public class AppTest {
         assertEquals(practiceUrl, currentUrl);
     }
 
-    @Test
-    public void shouldAnswerWithTrue2() throws MalformedURLException {
-        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-        capabilities.setBrowserName("firefox");
-        capabilities.setPlatform(Platform.LINUX);
-        driver = new RemoteWebDriver(new URL(nodeUrl), capabilities);
-        String practiceUrl = "https://www.bbc.com/";
-        driver.get(practiceUrl);
-        String currentUrl = driver.getCurrentUrl();
-        System.out.println(currentUrl);
-        driver.quit();
-        assertEquals(practiceUrl, currentUrl);
-    }
+//    @Test
+//    public void shouldAnswerWithTrue2() throws MalformedURLException {
+//        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+//        capabilities.setBrowserName("firefox");
+//        capabilities.setPlatform(Platform.LINUX);
+//        driver = new RemoteWebDriver(new URL(nodeUrl), capabilities);
+//        String practiceUrl = "https://www.bbc.com/";
+//        driver.get(practiceUrl);
+//        String currentUrl = driver.getCurrentUrl();
+//        System.out.println(currentUrl);
+//        driver.quit();
+//        assertEquals(practiceUrl, currentUrl);
+//    }
 }
 
 
