0b282b728b0290fb40f6b6c06b23f2ab5942778c
bezdomnij <attila.novak@yahoo.co.uk>
2021-03-08 15:30:24 +0100

    updated Test class for parallell run

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -4,12 +4,15 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 
-
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import sun.security.krb5.internal.crypto.Des;
 
 
 import java.net.MalformedURLException;
@@ -20,28 +23,39 @@ public class AppTest
 {
     static WebDriver driver;
 
-    /*@BeforeTest
+    @BeforeTest
     public void setUp()throws MalformedURLException {
-        final String WEBDRIVER = "webdriver.chrome.driver";
-        final String CHROME_DRIVER_LOCATION = "/home/david/Letöltések";
-        System.setProperty(WEBDRIVER, CHROME_DRIVER_LOCATION);
+//        final String WEBDRIVER = "webdriver.chrome.driver";
+//        final String CHROME_DRIVER_LOCATION = "/home/david/Letöltések";
+//        System.setProperty(WEBDRIVER, CHROME_DRIVER_LOCATION);
+//
         String nodeUrl = "https://seleniumhub.codecool.codecanvas.hu/wd/hub";
-        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-        capabilities.setBrowserName("chrome");
-        capabilities.setPlatform(Platform.LINUX);
+        String browser = System.getenv("BROWSER");
+        DesiredCapabilities capabilities;
+        if (browser.equals("chrome")) {
+            capabilities = DesiredCapabilities.chrome();
+            capabilities.setBrowserName("chrome");
+            capabilities.setPlatform(Platform.LINUX);
+        } else if (browser.equals("firefox")) {
+            capabilities = DesiredCapabilities.firefox();
+            capabilities.setBrowserName("firefox");
+            capabilities.setPlatform(Platform.LINUX);
+        } else {
+            capabilities = null;
+        }
 
         driver = new RemoteWebDriver(new URL(nodeUrl), capabilities);
-    }*/
+    }
 
 
     @Test
     public void shouldAnswerWithTrue() throws MalformedURLException {
-        String nodeUrl = "https://selenium:CoolCanvas19.@seleniumhub.codecool.codecanvas.hu/wd/hub";
-        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-        capabilities.setBrowserName("chrome");
-        capabilities.setPlatform(Platform.LINUX);
+//        String nodeUrl = "https://selenium:CoolCanvas19.@seleniumhub.codecool.codecanvas.hu/wd/hub";
+//        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+//        capabilities.setBrowserName("chrome");
+//        capabilities.setPlatform(Platform.LINUX);
+//        driver = new RemoteWebDriver(new URL(nodeUrl), capabilities);
 
-        driver = new RemoteWebDriver(new URL(nodeUrl), capabilities);
         String practiceUrl = "https://www.google.com/";
         driver.get(practiceUrl);
         String currentUrl = driver.getCurrentUrl();
@@ -50,14 +64,15 @@ public class AppTest
         assertEquals(practiceUrl, currentUrl);
     }
 
+    @Ignore
     @Test
     public void shouldAnswerWithTrue2() throws MalformedURLException {
-        String nodeUrl = "https://selenium:CoolCanvas19.@seleniumhub.codecool.codecanvas.hu/wd/hub";
-        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-        capabilities.setBrowserName("firefox");
-        capabilities.setPlatform(Platform.LINUX);
+//        String nodeUrl = "https://selenium:CoolCanvas19.@seleniumhub.codecool.codecanvas.hu/wd/hub";
+//        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+//        capabilities.setBrowserName("firefox");
+//        capabilities.setPlatform(Platform.LINUX);
+//        driver = new RemoteWebDriver(new URL(nodeUrl), capabilities);
 
-        driver = new RemoteWebDriver(new URL(nodeUrl), capabilities);
         String practiceUrl = "https://www.google.com/";
         driver.get(practiceUrl);
         String currentUrl = driver.getCurrentUrl();
@@ -66,10 +81,10 @@ public class AppTest
         assertEquals(practiceUrl, currentUrl);
     }
 
-    /*@AfterTest
+    @AfterTest
     public void afterTest(){
         driver.quit();
-    }*/
+    }
 }
 
 
