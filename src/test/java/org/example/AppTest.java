52445db5c5f48eea6f0d7c0146ee301cbeb84caf
bezdomnij <attila.novak@yahoo.co.uk>
2021-03-08 16:08:25 +0100

    updated Test class for parallell run

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -24,38 +24,38 @@ public class AppTest
     static WebDriver driver;
     private final String nodeUrl = "http://localhost:4446/wd/hub";
 
-    @BeforeTest
-    public void setUp()throws MalformedURLException {
-
-//        final String WEBDRIVER = "webdriver.chrome.driver";
-//        final String CHROME_DRIVER_LOCATION = "/home/david/Letöltések";
-//        System.setProperty(WEBDRIVER, CHROME_DRIVER_LOCATION);
+//    @BeforeTest
+//    public void setUp()throws MalformedURLException {
 //
-        String browser = System.getenv("BROWSER");
-        DesiredCapabilities capabilities;
-        if (browser.equals("chrome")) {
-            capabilities = DesiredCapabilities.chrome();
-            capabilities.setBrowserName("chrome");
-            capabilities.setPlatform(Platform.LINUX);
-        } else if (browser.equals("firefox")) {
-            capabilities = DesiredCapabilities.firefox();
-            capabilities.setBrowserName("firefox");
-            capabilities.setPlatform(Platform.LINUX);
-        } else {
-            capabilities = null;
-        }
-
-        driver = new RemoteWebDriver(new URL(nodeUrl), capabilities);
-    }
-
+////        final String WEBDRIVER = "webdriver.chrome.driver";
+////        final String CHROME_DRIVER_LOCATION = "/home/david/Letöltések";
+////        System.setProperty(WEBDRIVER, CHROME_DRIVER_LOCATION);
+////
+//        String browser = System.getenv("BROWSER");
+//        DesiredCapabilities capabilities;
+//        if (browser.equals("chrome")) {
+//            capabilities = DesiredCapabilities.chrome();
+//            capabilities.setBrowserName("chrome");
+//            capabilities.setPlatform(Platform.LINUX);
+//        } else if (browser.equals("firefox")) {
+//            capabilities = DesiredCapabilities.firefox();
+//            capabilities.setBrowserName("firefox");
+//            capabilities.setPlatform(Platform.LINUX);
+//        } else {
+//            capabilities = null;
+//        }
+//
+//        driver = new RemoteWebDriver(new URL(nodeUrl), capabilities);
+//    }
+//
 
     @Test
     public void shouldAnswerWithTrue() throws MalformedURLException {
-//        String nodeUrl = "https://selenium:CoolCanvas19.@seleniumhub.codecool.codecanvas.hu/wd/hub";
-//        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-//        capabilities.setBrowserName("chrome");
-//        capabilities.setPlatform(Platform.LINUX);
-//        driver = new RemoteWebDriver(new URL(nodeUrl), capabilities);
+        String nodeUrl = "https://selenium:CoolCanvas19.@seleniumhub.codecool.codecanvas.hu/wd/hub";
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setBrowserName("chrome");
+        capabilities.setPlatform(Platform.LINUX);
+        driver = new RemoteWebDriver(new URL(nodeUrl), capabilities);
 
         String practiceUrl = "https://www.google.com/";
         driver.get(practiceUrl);
@@ -65,27 +65,27 @@ public class AppTest
         assertEquals(practiceUrl, currentUrl);
     }
 
-//    @Ignore
-//    @Test
-//    public void shouldAnswerWithTrue2() throws MalformedURLException {
-////        String nodeUrl = "https://selenium:CoolCanvas19.@seleniumhub.codecool.codecanvas.hu/wd/hub";
-////        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-////        capabilities.setBrowserName("firefox");
-////        capabilities.setPlatform(Platform.LINUX);
-////        driver = new RemoteWebDriver(new URL(nodeUrl), capabilities);
-//
-//        String practiceUrl = "https://www.google.com/";
-//        driver.get(practiceUrl);
-//        String currentUrl = driver.getCurrentUrl();
-//        System.out.println(currentUrl);
-//        driver.quit();
-//        assertEquals(practiceUrl, currentUrl);
-//    }
 
-    @AfterTest
-    public void afterTest(){
+    @Test
+    public void shouldAnswerWithTrue2() throws MalformedURLException {
+        String nodeUrl = "https://selenium:CoolCanvas19.@seleniumhub.codecool.codecanvas.hu/wd/hub";
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        capabilities.setBrowserName("firefox");
+        capabilities.setPlatform(Platform.LINUX);
+        driver = new RemoteWebDriver(new URL(nodeUrl), capabilities);
+
+        String practiceUrl = "https://www.google.com/";
+        driver.get(practiceUrl);
+        String currentUrl = driver.getCurrentUrl();
+        System.out.println(currentUrl);
         driver.quit();
+        assertEquals(practiceUrl, currentUrl);
     }
+
+//    @AfterTest
+//    public void afterTest(){
+//        driver.quit();
+//    }
 }
 
 
